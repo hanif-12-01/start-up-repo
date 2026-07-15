cf83828bb0604b5a561c8e5d44690206b56ebb8c
hanifalfaiz <stshfaiz@gmail.com>
2026-07-15 14:48:08 +0700

    feat(demo): configure prediction phase accounts

--- wattwise-laravel/config/demo.php
+++ wattwise-laravel/config/demo.php
@@ -2,4 +2,9 @@
 
 return [
     'enabled' => (bool) env('DEMO_LOGIN_ENABLED', false),
+
+    'phase_accounts' => [
+        'enabled' => (bool) env('DEMO_PHASE_ACCOUNTS_ENABLED', false),
+        'password' => env('DEMO_PHASE_ACCOUNTS_PASSWORD'),
+    ],
 ];
